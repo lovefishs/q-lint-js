33b2b00201545efede6a00fb709b5f7ddc49c79b
changming.huang <changming.huang@woqutech.com>
2017-07-03 14:31:53 +0800

    options: remove homepage and bugs prop

--- options.js
+++ options.js
@@ -3,13 +3,11 @@ const eslint = require('eslint')
 const pkg = require('./package.json')
 
 module.exports = {
-  bugs: pkg.bugs.url,
   cmd: 'q-lint-js',
   eslint: eslint,
   eslintConfig: {
     configFile: resolve(__dirname, './eslintrc.json'),
   },
-  homepage: pkg.homepage,
   tagline: 'Use JavaScript Q Style',
   version: pkg.version,
 }
